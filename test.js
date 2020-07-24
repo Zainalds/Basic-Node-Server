10b589008e8e676e44a8e2dcdeb68b1eae72cb4b
Zainalds <68240955+Zainalds@users.noreply.github.com>
2020-07-24 16:06:10 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -1,12 +1,33 @@
-let http = require("http")
+let express = require("express")
+let ourApp = express()
 
-let ourApp = http.createServer(function(req, res){
-    if(req.url == "/"){
-        res.end("Welcome to our home page")
+
+ourApp.use(express.urlencoded({extended: false}))
+
+
+//What to do when the browser receives a get request.
+ourApp.get('/', function(req, res){
+    res.send(`
+        <form action="/answer" method="POST">
+            <p>What color is the sky on a clear and sunny day?</p>
+            <input type="text" name="skyColor" autocomplete="off">
+            <button>Submit Answer</button>
+        </form>
+    
+    `)
+})
+//What to do when the browser receives information from a get request
+ourApp.post('/answer', function(req, res){
+    if(req.body.skyColor.toUpperCase() == "BLUE"){
+        res.send(`
+            <p>Congrats, that is the correct answer</p>
+            <a href="/" >Back to home page</a>
+        `)
+    }else{
+        res.send(`
+        <p>Sorry that is incorrect</p>
+        <a href="/" >Back to home page</a>
+    `)
     }
-    if(req.url == "/about"){
-        res.end("Thank you for the interest in our company")
-    }   
-    res.end("We cannot find the page you are looking for")
 })
 ourApp.listen(3000)
